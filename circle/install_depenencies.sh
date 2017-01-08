1353f52891d471e8c4340f60f3633c712a6d9db3
saantiaguilera <santiago.aguilera@mercadolibre.com>
2017-01-08 16:08:19 -0300

    CI

--- circle/install_depenencies.sh
+++ circle/install_depenencies.sh
@@ -5,9 +5,8 @@ export PATH="$ANDROID_HOME/platform-tools:$ANDROID_HOME/tools:$PATH"
 DEPS="$ANDROID_HOME/installed-dependencies"
 
 if [ ! -e $DEPS ]; then
-    cp -r /usr/local/android-sdk-linux $ANDROID_HOME &&
-        echo y | android update sdk -u -a -t build-tools-20.0.0 &&
-        echo y | android update sdk -u -a -t android-19 &&
+    echo y | android update sdk -u -a -t build-tools-25.0.1 &&
+        echo y | android update sdk -u -a -t android-25 &&
         echo y | android update sdk -u -a -t extra-android-m2repository &&
         echo y | android update sdk -u -a -t extra-google-m2repository &&
         touch $DEPS
